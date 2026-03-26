2219734162e24cb6d874295f7933973a2e0e62ca
AnicheevA-UMD <anicheev@umd.edu>
2026-03-26 14:17:55 -0400

    Main.py done

--- src/main.py
+++ src/main.py
@@ -6,8 +6,8 @@ import pandas as pd
 import part1_etl
 import part2_preprocessing
 import part3_logistic_regression
-
-
+import part4_decision_tree
+import part5_calibration_plot
 
 def main():
     # PART 1: Instanciate etl, saving the two datasets in `./data/`
@@ -19,12 +19,12 @@ def main():
     df_arrests = part2_preprocessing.wrapper()
 
     # PART 3: Call functions/instanciate objects from logistic_regression
-    df_arrests_train, df_arrests_test = part3_logistic_regression.logistic_regression(df_arrests)
+    df_arrests_train, df_arrests_test, gs_cv = part3_logistic_regression.logistic_regression(df_arrests)
 
     # PART 4: Call functions/instanciate objects from decision_tree
-
+    df_arrests_train, df_arrests_test, gs_cv_dt = part4_decision_tree.decision_tree(df_arrests_train, df_arrests_test)
     # PART 5: Call functions/instanciate objects from calibration_plot
-
-
+    part5_calibration_plot.calibration_plots(df_arrests_test, gs_cv, gs_cv_dt)
+    
 if __name__ == "__main__":
     main()
